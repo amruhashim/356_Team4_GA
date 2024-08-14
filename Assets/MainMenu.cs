6a6b347e17bd75291d67d981130dcf9080f14daa
krustydoescoding <ong_qi_lun@hotmail.com>
2024-08-14 22:44:43 +0800

    Hostage updated

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -4,15 +4,13 @@ using UnityEngine;
 
 public class MainMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void NewGame()
     {
-        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ExitGame()
     {
-        
+
     }
 }
