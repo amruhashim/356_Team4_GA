0f8c253107aeba4b3386c95a26cc5fb5d2df4705
krustydoescoding <ong_qi_lun@hotmail.com>
2024-08-15 16:42:26 +0800

    Working Main Menu

--- Assets/Scripts/QiLun/MainMenu.cs
+++ Assets/Scripts/QiLun/MainMenu.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
     public void NewGame()
     {
-
+        SceneManager.LoadScene("ToTest");
     }
 
     public void ExitGame()
     {
-
+        Debug.Log("Quitting Game");
+        Application.Quit();
     }
 }
