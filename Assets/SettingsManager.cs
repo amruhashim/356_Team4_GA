74f96f38ba97e0a06f96c41e12c151525bfba89b
krustydoescoding <ong_qi_lun@hotmail.com>
2024-08-17 14:32:45 +0800

    Volume Settings update

---
+++ Assets/SettingsManager.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using static MainMenuSaveManager;
+
+public class SettingsManager : MonoBehaviour
+{
+    public static SettingsManager Instance { get; set; }
+
+    public Button backBTN;
+
+    public Slider masterSlider;
+    public GameObject masterValue;
+
+    public Slider musicSlider;
+    public GameObject musicValue;
+
+    public Slider effectsSlider;
+    public GameObject effectsValue;
+
+    private void Start()
+    {
+        backBTN.onClick.AddListener(() =>
+        {
+
+            MainMenuSaveManager.Instance.SaveVolumeSettings(musicSlider.value, effectsSlider.value, masterSlider.value);
+        });
+
+        StartCoroutine(LoadAndApplySettings());
+    }
+
+    private IEnumerator LoadAndApplySettings()
+    {
+        LoadAndSetVolume();
+
+        //Load GraphicsSettings method
+        //Load Key Bindings method
+
+        yield return new WaitForSeconds(0.1f);
+    }
+
+    private void LoadAndSetVolume()
+    {
+        VolumeSettings volumeSettings = MainMenuSaveManager.Instance.LoadVolumeSettings();
+
+        masterSlider.value = volumeSettings.master;
+        musicSlider.value = volumeSettings.music;
+        effectsSlider.value = volumeSettings.effects;
+
+        print("Volume Settings are Loaded");
+    }
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+    }
+
+    private void Update()
+    {
+        masterValue.GetComponent<TextMeshProUGUI>().text = "" + (masterSlider.value) + "";
+        musicValue.GetComponent<TextMeshProUGUI>().text = "" + (musicSlider.value) + "";
+        effectsValue.GetComponent<TextMeshProUGUI>().text = "" + (effectsSlider.value) + "";
+    }
+
+}
